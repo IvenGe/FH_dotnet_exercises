39c4deb856d7e821e74129290b080f29a7486cd9
iven germonpre <iven.germonpre@gmail.com>
2023-09-26 12:54:11 +0200

    Fixed so it does not need dot net 7 fix get help bug

--- crunchTheNumbers/nummberCrunch/Program.cs
+++ crunchTheNumbers/nummberCrunch/Program.cs
@@ -68,8 +68,7 @@ class NumberCrunch
 
     static void ShowHelp()
     {
-static void ShowHelp()
-{
+
     Console.WriteLine("You can enter simple calculations like '1+2' and let them calculate by pressing enter.");
     Console.WriteLine("You can only use one operand at a time. If you want to use negative numbers, you need to store them in a variable first.");
     Console.WriteLine("The operands you can use are +, -, *, /, and ^. Furthermore, you can assign a variable a value by using a syntax like 'hello=15'.");
@@ -78,7 +77,7 @@ static void ShowHelp()
     Console.WriteLine("The result of the last calculation will always be available under the variable 'ans'.");
     Console.WriteLine("You can display the variables with the command '/list'. You can clear them by using '/clear'.");
     Console.WriteLine("To stop the Crunch the Numbers calculation service, use '/stop'. To display this help text at any time use '/help'");
-}
+
 
     }
 
