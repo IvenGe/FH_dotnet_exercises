4e9a06dc26d858f98bfae70d362cd53c6ccc8709
aherman21 <hermansalexander2@gmail.com>
2023-11-13 22:32:31 +0100

    commit after mac update

--- Blog/Business/Post/GetPosts.cs
+++ Blog/Business/Post/GetPosts.cs
@@ -6,16 +6,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Blog.API.Business.Post;
 
-public record GetPosts : IQuery<GetPosts.Result>
+public record GetPosts(string? Name = null) : IQuery<GetPosts.Result>
 {
-public record Result(IEnumerable<PostDto> Items);
-public class Handler : IRequestHandler<GetPosts, Result>
-    {
-    private readonly PostInfoContext context;
-    public Handler(PostInfoContext context) => this.context = context;
-    public async Task<Result> Handle(GetPosts request, CancellationToken
-    cancellationToken)
-    => new Result(await context.Posts.Include(x => x.Comments)
-    .Select(x => new PostDto(x)).ToListAsync(cancellationToken));
-}
+    public record Result(IEnumerable<PostDto> Items);
+    public class Handler : IRequestHandler<GetPosts, Result>
+        {
+        private readonly PostInfoContext context;
+        public Handler(PostInfoContext context) => this.context = context;
+        public async Task<Result> Handle(GetPosts request, CancellationToken
+        cancellationToken)
+        {
+            var queryable = context.Posts.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(request.Name))
+            {
+                queryable = queryable.Where(x => x.Name == request.Name);
+            }
+
+            return new Result(await queryable.Select(x => new PostDto(x))
+                .ToListAsync(cancellationToken));
+        }
+    }
 }
